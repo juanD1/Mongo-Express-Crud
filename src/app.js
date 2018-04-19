83291b30c597400d5c797c4a1c997659e7adfea7
JuanD1 <juandacor@outlook.com>
2018-04-19 17:30:17 -0500

    configurando routes v1 y app.js

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+const indexRoutes = require('./routes/')
+
+//Settings
+app.set('port', process.env.PORT || 3000)
+
+//middlewares
+app.use(logger('dev'))
+app.use(bodyParser.urlencoded({extended: false}))
+
+//routes
+app.use('/',indexRoutes)
+
+app.listen(app.get('port'), () => {
+	console.log('server on port ', app.get('port'))
+})
